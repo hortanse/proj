89eb83de0c89abdc1219b21fb25bd5c37eb65cf3
Yi-Fan Chou <hortanse@gmail.com>
2016-06-20 23:04:48 -0400

    add q6.pl

--- week03/q6.pl
+++ week03/q6.pl
@@ -1,11 +1,22 @@
-#! /usr/bin/perl -w
+#! /usr/bin/perl
 use strict;
 use warnings;
 
-open( my $IN , '<' , $file ) or die( "can't open $file ($!)" );
-while( <$IN> ) {
-  chomp;
-  die "failed to match:\n$_\n" unless /REGEXP/;
+print "Enter regexp : ";
+my $regexp = <>;
+chomp($regexp);
+
+while (1) {
+    print "Enter string or 'exit' to exit : ";
+        my $string = <>;
+        chomp($string);
+        last if $string =~ /exit/;            # matching exit to exit loop
+
+        if ($string =~ /$regexp/) {           # test matching
+            print "$string Match! \n";        # print out matched result
+        }
+        else {
+            print "$string No match! \n";     # print out no match result
+        }
 }
-close( $IN );
-print "everything matched.\n";
+print "exit bye bye \n";
